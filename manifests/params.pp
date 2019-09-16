f835e5d31100651d94e678e124c4094b23e2ec73
markasammut <mark.sammut@gamesysnetwork.com>
2019-09-16 15:29:39 +0200

    Create params.pp

---
+++ manifests/params.pp
@@ -0,0 +1,19 @@
+  
+# == Class: incron::params
+#
+# Defines defaults for the incron class. Params class should not be called
+# directly.
+#
+class incron::params {
+
+  case $::osfamily {
+    'Debian': {
+      $service_name = 'incron'
+    }
+
+    default: {
+      $service_name = 'incrond'
+    }
+  }
+
+}
